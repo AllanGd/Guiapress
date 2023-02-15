b751daf6c397233130138f7328834c5049cbd5eb
AllanGd <allan_gd@hotmail.com>
2023-02-14 22:25:56 -0300

    articles pages created

--- articles/articlesController.js
+++ articles/articlesController.js
@@ -1,12 +1,36 @@
 const express = require("express");
 const router = express.Router();
+const Category = require('../categories/Category');
+const Article = require('./Article');
+const slugify = require('slugify');
 
-router.get("/articles", (req,res)=>{
-    res.send("rota categorias")
+router.get("/admin/articles", (req,res)=>{
+    Article.findAll({
+        include: [{model: Category}]
+    }).then(articles =>{
+        res.render("admin/articles/index",{articles: articles});
+    })
 });
 
 router.get("/admin/articles/new", (req,res)=>{
-    res.send("rota para criar uma nova artigo")
+    Category.findAll().then(categories => {
+        res.render("admin/articles/new",{categories: categories})
+    })
 });
+
+router.post("/articles/save", (req,res) => {
+    var title = req.body.title;
+    var body = req.body.body;
+    var category = req.body.category;
+
+    Article.create({
+        title: title,
+        slug: slugify(title),
+        body: body,
+        categoryId: category
+    }).then(() => {
+        res.redirect("/admin/article");
+    })
+})
 
 module.exports = router;
--- database/database.js
+++ database/database.js
@@ -3,7 +3,8 @@ const Sequelize = require("sequelize");
 const connection = new Sequelize('guiapress','postgres','a1@a2ccc3',{
     host: 'localhost',
     dialect: 'postgres',
-    logging: false
+    logging: false,
+    timezone: "-3:00"
 });
 
 module.exports = connection;
