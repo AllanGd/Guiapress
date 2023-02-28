cde2d6fa3e2201a4f38b3f34357f0273dadb24f6
AllanGd <allan_gd@hotmail.com>
2023-02-27 23:45:44 -0300

    article view create

--- index.js
+++ index.js
@@ -8,35 +8,54 @@ const articlesController = require("./articles/articlesController");
 
 const Article = require("./articles/Article");
 const Category = require("./categories/Category");
+const { render } = require("ejs");
 
 //View engine
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
 //Static
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 //Body parser
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 
 //Database
 connection
-    .authenticate()
-    .then(()=>{
-        console.log("Conexão feita com sucesso!");
-    }).catch((error)=>{
-        console.log(error);
-    })
+  .authenticate()
+  .then(() => {
+    console.log("Conexão feita com sucesso!");
+  })
+  .catch((error) => {
+    console.log(error);
+  });
 
 app.use("/", categoriesController);
 app.use("/", articlesController);
 
 app.get("/", (req, res) => {
-    Article.findAll().then(articles => {
-        res.render("index",{articles: articles})
-    })
+  Article.findAll().then((articles) => {
+    res.render("index", { articles: articles });
+  });
 });
 
-app.listen(8080, ()=>{
-    console.log("O servidor está rodando!");
+app.get("/:slug", (req, res) => {
+  var slug = req.params.slug;
+  Article.findOne({
+    where: {
+      slug: slug
+    }
+  }).then(article => {
+      if (article != undefined) {
+        res, render("article", { article: article });
+      } else {
+        res.redirect("/");
+      }
+    }).catch((err) => {
+      res.redirect("/");
+    });
+});
+
+app.listen(8080, () => {
+  console.log("O servidor está rodando!");
 });
